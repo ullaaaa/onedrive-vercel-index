fe2c8f29822f64db10f64da017ae331a73d0595d
ullaaaa <60058657+ullaaaa@users.noreply.github.com>
2023-02-07 16:46:20 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -41,6 +41,7 @@ module.exports = {
   // the directories in which you have .password set. Check the documentation for details.
   "protectedRoutes": [
   "/bt_backup",
+  "/Archive/工程",
 ],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
